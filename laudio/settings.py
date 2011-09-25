ef69a1278e6db9f56d5bcc67e20d1d0793ac8ae4
Bernhard Posselt <bernhard.posselt@gmx.at>
2011-09-25 17:25:48 +0200

    updated settings file, broke app

--- laudio/settings.py
+++ laudio/settings.py
@@ -20,35 +20,43 @@ along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
 
 """
 
+# imports
 import os.path
 
-from django.core.urlresolvers import reverse
 
-# Django settings for laudio project.
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@domain.com'),
+    # ('Your Name', 'your_email@example.com'),
 )
-AUTH_PROFILE_MODULE = 'laudio.UserProfile'
 
 MANAGERS = ADMINS
 
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/var/lib/laudio/laudio.db',
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '/var/lib/laudio/laudio.db',    # Or path to database file if using sqlite3.
+        
+        """uncomment the following lines if you want to use a different
+        database such as mysql
+        
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        """
     }
 }
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
+# On Unix systems, a value of None will cause Django to use the same
+# timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'Europe/Vienna'
+TIME_ZONE = 'America/Chicago'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -60,36 +68,62 @@ SITE_ID = 1
 # to load the internationalization machinery.
 USE_I18N = True
 
-# Absolute path to the directory that holds media.
-# Example: "/home/media/media.lawrence.com/"
+# If you set this to False, Django will not format dates, numbers and
+# calendars according to the current locale
+USE_L10N = True
 
-LAUDIO_VERSION = "0.5.2.1"
-LAST_FM_API_KEY = "a1d1111ab0b08262e6d7484cc5dc949a"
+# Shortcut for the installation directory
 INSTALL_DIR = os.path.dirname( os.path.abspath(__file__) )
-MEDIA_ROOT = os.path.join( os.path.dirname( os.path.abspath(__file__) ), 'media/')
-AUDIO_DIR = os.path.join( os.path.dirname( os.path.abspath(__file__) ), 'media/audio')
-DEBUG_LOG = "/var/log/laudio/debug.log"
-SCAN_LOG = "/var/log/laudio/scan.log"
+
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/home/media/media.lawrence.com/media/"
+MEDIA_ROOT = ''
+
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash if there is a path component (optional in other cases).
-# Examples: "http://media.lawrence.com", "http://example.com/media/"
+# trailing slash.
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
 MEDIA_URL = ''
 
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
-# trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/admin/'
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = os.path.join( os.path.dirname( os.path.abspath(__file__) ), 'static/')
+
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+STATIC_URL = '/static/'
+
+# URL prefix for admin static files -- CSS, JavaScript and images.
+# Make sure to use a trailing slash.
+# Examples: "http://foo.com/static/admin/", "/static/admin/".
+ADMIN_MEDIA_PREFIX = '/static/admin/'
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
+
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = '9e2ul*e7diorgb!$#546hiotf6zqh=zf9ij)*_km9$g-%pacy8'
+SECRET_KEY = 'h6%=n5c(b(9u@-^@0ke^u@=y%r-e1ke)13^c2325gn!w#=c3=f'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
-#     'django.template.loaders.eggs.load_template_source',
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+#     'django.template.loaders.eggs.Loader',
 )
-
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
@@ -113,5 +147,51 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
     'laudio',
+    # Uncomment the next line to enable the admin:
+    # 'django.contrib.admin',
+    # Uncomment the next line to enable admin documentation:
+    # 'django.contrib.admindocs',
 )
+
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
+
+########################
+# laudio specific values
+
+# The file where debug information is being stored in
+DEBUG_LOG = "/var/log/laudio/debug.log"
+
+# This file is used for logging already scanned file, necessary for
+# displaying the progressbar.
+# TODO: implement a more elegant solution
+SCAN_LOG = "/var/log/laudio/scan.log"
+
+# Change this for changing the version in the about page
+LAUDIO_VERSION = "0.5.2.1"
+
+# API key for last.fm, needed for scrobbling
+LAST_FM_API_KEY = "a1d1111ab0b08262e6d7484cc5dc949a"
