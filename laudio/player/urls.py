ceaddce625e2b9618c67efe2b9e23fce974d2535
Bernhard Posselt <bernhard.posselt@gmx.at>
2011-09-25 21:51:00 +0200

    changed naming scheme for urls

--- laudio/player/urls.py
+++ laudio/player/urls.py
@@ -21,57 +21,47 @@ along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 from django.conf.urls.defaults import *
-from laudio.views import *
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from laudio.player.views import *
+from laudio.player.ajax import *
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^laudio/', include('laudio.foo.urls')),
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
     
     # settings
-    (r'^settings/$', laudio_settings),
-    (r'^settings/resetdb/$', ajax_drop_collection_db),
-    (r'^settings/scan/$', ajax_scan_collection),
-    (r'^settings/newuser/$', laudio_settings_new_user),
-    (r'^settings/deleteuser/(?P<userid>.*)/$', laudio_settings_delete_user),
-    (r'^settings/edituser/(?P<userid>.*)/$', laudio_settings_edit_user),
+    (r'^settings/$', settings, name='settings'),
+    (r'^settings/db/reset/$', settings_db_reset, name='settings_db_reset'),
+    (r'^settings/db/scan/$', settings_db_scan, name='settings_db_scan'),
+    (r'^settings/db/scan/info/$',settings_db_scan_info, name='settings_db_scan_info'),
+    (r'^settings/user/new/$', settings_user_new, name='settings_user_new'),
+    (r'^settings/user/delete/(?P<userid>.*)/$', settings_user_delete, name='settings_user_delete'),
+    (r'^settings/user/edit/(?P<userid>.*)/$', settings_user_edit, name='settings_user_edit'),
     
     # collection
-    (r'^$', laudio_index),
-    (r'^collection/$', ajax_whole_collection),
-    (r'^artist/(?P<artist>.*)/$', ajax_artists_by_letters),
-    (r'^searchall/(?P<search>.*)/$', ajax_search_collection),
-    (r'^advsearch/$', ajax_adv_search_collection),
-    (r'^song_data/(?P<id>.*)/$', ajax_song_metadata),
-    (r'^song_download/(?P<id>.*)/$', ajax_song_download),
-    (r'^scrobble/(?P<id>.*)/$', ajax_scrobble_song),
-    (r'^cover/(?P<id>.*)/$', ajax_cover_fetch),
-    (r'^advautocomplete/(?P<row>.*)/$', ajax_adv_autocompletion),
-    (r'^settings/scan/info/$', ajax_scan_perc),
+    (r'^$', index),
+    # ajax requests for collection
+    (r'^collection/$', index_collection, name='index_collection'),
+    (r'^artist/letter/(?P<artist>.*)/$', index_artist_letter, name='index_artist_letter'),
+    (r'^search/all/(?P<search>.*)/$', index_search, name='index_search'),
+    (r'^search/advanced/$', index_search_advanced, name='index_search_advanced'),
+    (r'^song/meta/(?P<id>.*)/$', index_song_meta, name='index_song_meta'),
+    (r'^song/download/(?P<id>.*)/$', index_song_download, name='index_song_download'),
+    (r'^scrobble/(?P<id>.*)/$', index_scrobble, name='index_scrobble'),
+    (r'^cover/(?P<id>.*)/$', index_cover, name='index_cover'),
+    (r'^autocomplete/(?P<row>.*)/$', index_autocomplete, name='index_autocomplete'),
     
     # playlist requests
-    (r'^playlist/save/(?P<playlistName>.*)/$', save_playlist),
-    (r'^playlist/exists/(?P<playlistName>.*)/$', playlist_exists),
-    (r'^playlist/getname/(?P<playlistId>.*)/$', get_playlist_name),
-    (r'^playlist/open/(?P<playlistId>.*)/$', open_playlist),
-    (r'^playlist/delete/(?P<playlistId>.*)/$', delete_playlist),
-    (r'^playlist/rename/(?P<oldName>.*)/(?P<newName>.*)/$', rename_playlist),
-    (r'^playlist/list/$', list_playlists),
+    (r'^playlist/save/(?P<playlistName>.*)/$', playlist_save, name='playlist_save'),
+    (r'^playlist/exists/(?P<playlistName>.*)/$', playlist_exists, name='playlist_exists'),
+    (r'^playlist/name/(?P<playlistId>.*)/$', playlist_name, name='playlist_name'),
+    (r'^playlist/open/(?P<playlistId>.*)/$', playlist_open, name='playlist_open'),
+    (r'^playlist/delete/(?P<playlistId>.*)/$', playlist_delete, name='playlist_delete'),
+    (r'^playlist/rename/(?P<oldName>.*)/(?P<newName>.*)/$', playlist_rename, name='playlist_rename'),
+    (r'^playlist/list/$', playlist_list, name='playlist_list'),
     
     # other sites
-    (r'^about/$', laudio_about),
-    (r'^profile/$', laudio_profile),
+    (r'^about/$', about),
+    (r'^profile/$', profile),
     (r'^chat/$', 'django.views.generic.simple.direct_to_template', {'template': 'chat.html'}),
     (r'^login/', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
     (r'^logout/$', 'django.contrib.auth.views.logout', {'template_name': 'logout.html'}),
+
 )
