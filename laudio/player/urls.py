1e0be9991484d4232c86b153ba1f56f254ab4cbe
Bernhard Posselt <bernhard.posselt@gmx.at>
2011-12-25 13:22:00 +0100

    added all links to settings page

--- laudio/player/urls.py
+++ laudio/player/urls.py
@@ -56,10 +56,10 @@ urlpatterns += patterns('laudio.player.views.config',
     # settings and profile
     url(r'^config/settings/$', 'config_settings', name='config_settings'),
     url(r'^config/profile/$', 'config_profile', name='config_profile'),
-    url(r'^config/settings/new/user/(?P<userid>\d+)/$', 'config_settings_new_user', name='config_settings_new_user'),
+    url(r'^config/settings/new/user/$', 'config_settings_new_user', name='config_settings_new_user'),
     url(r'^config/settings/edit/user/(?P<userid>\d+)/$', 'config_settings_edit_user', name='config_settings_edit_user'),
     url(r'^config/settings/delete/user/(?P<userid>\d+)/$', 'config_settings_delete_user', name='config_settings_delete_user'),
-    url(r'^config/settings/xml/new/user/(?P<userid>\d+)/$', 'xml_config_settings_new_user', name='xml_config_settings_new_user'),
+    url(r'^config/settings/xml/new/user/$', 'xml_config_settings_new_user', name='xml_config_settings_new_user'),
     url(r'^config/settings/xml/edit/user/(?P<userid>\d+)/$', 'xml_config_settings_edit_user', name='xml_config_settings_edit_user'),
     url(r'^config/settings/xml/delete/user/(?P<userid>\d+)/$', 'xml_config_settings_delete_user', name='xml_config_settings_delete_user'),
 )
