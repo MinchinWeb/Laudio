5d352e31dd86e9cffebbb02a39b4aff39f8e239c
Bernhard Posselt <bernhard.posselt@gmx.at>
2012-07-22 02:38:08 +0200

    added new experimental setup file

--- setup.sh
+++ setup.sh
@@ -16,187 +16,216 @@
 # You should have received a copy of the GNU General Public License
 # along with Laudio.  If not, see <http://www.gnu.org/licenses/>.
 
-
-# general constants
-INSTALL_DIR="/usr/share/laudio"
-LAUDIO_CONF_DIR="/etc/laudio"
-STANDARD_HTTP_USER="www-data"
-CREATE_DIRS=(   "/usr/share/laudio" 
+install_dir="/usr/share/laudio"
+config_dir="/etc/laudio"
+needed_dirs=(   "/usr/share/laudio" 
                 "/var/lib/laudio" 
                 "/var/log/laudio"
                 "/etc/laudio"
                 "/etc/laudio/apache"
                 "/etc/laudio/lighttpd"
         )
-DATABASE_FILE="/var/lib/laudio/laudio.db"
+database_path="/var/lib/laudio/laudio.db"
 
-# arch linux specific contents
-ARCH_HTTP_USER="http"
+# Please fix if changed or wrong
+debian_deps="python-lxml python-django python-mutagen apache2 sqlite3 libapache2-mod-wsgi python-pysqlite2 ttf-dejavu ffmpeg"
 
+arch_deps="django python-lxml mutagen apache python-pysqlite sqlite3 mod_wsgi ttf-dejavu ffmpeg"
 
-if [ "$(whoami)" != 'root' ]; then
-    echo "You have no permission to run $0 as non-root user."
-    exit 1;
+fedora_deps="Django python-lxml python-mutagen httpd python-sqlite3dbm mod_wsgi dejavu-fonts-common ffmpeg"
+
+gentoo_deps="dev-python/django dev-python/lxml media-libs/mutagen dev-python/pysqlite dev-db/sqlite www-apache/mod_wsgi www-servers/apache ttf-dejavu ffmpeg"
+
+general_deps="
+    python (2.7) \n
+    python-lxml \n
+    python-django (1.4) \n
+    python-mutagen \n
+    apache2 \n
+    sqlite3 \n
+    libapache2-mod-wsgi \n
+    python-pysqlite2 \n
+    ttf-dejavu \n
+    ffmpeg \n
+"
+
+# get apache user and group
+apache_ug="www-data"
+if grep -Fxq apache /etc/group; then
+   apache_ug=apache
+fi
+if grep -Fxq httpd /etc/group; then
+   apache_ug=httpd
 fi
 
-echo -e "\n\n\n                     WARNING\n\n"
-echo "#####################################################"
-echo "# This install script hasnt been thouroughly tested!#"
-echo "# You have been warned.                             #"
-echo "# Please read and check it before you execute it!   #"
-echo -e "#####################################################\n"
+# check for distro
+distro="unknown"
+if [ -d /etc/system-release ]; then 
+  if grep -Fxq Fedora /etc/system-release; then
+     $distro="fedora"
+  fi
+fi
+# TODO: check for other distros
 
-echo "Please select your distribution and enter the number"
-echo "0) Ubuntu"
-echo "1) Arch Linux"
-echo "2) Gentoo"
-echo "3) Abort"
+# checks if we are root otherwise aborts the execution
+function require_root {
+   if [ "$(whoami)" != 'root' ]; then
+       echo "You have no permission to run $0 as non-root user."
+       exit 1;
+   fi
+}
 
-read DISTRO
+# creates directories
+function create_dirs {
+   for elem in ${needed_dirs[@]}; do
+      if ! [[ -e $elem ]]; then
+         mkdir -p $elem
+      fi
+   done
+}
 
+# creates the database
+function create_database {
+   python2 src/manage.py syncdb --noinput
+}
 
-case "$DISTRO" in
+# developement setup, dont use this in production!
+function setup_devel_rights {
+   # set every needed directory writable for all users
+   for elem in ${needed_dirs[@]}; do
+      if [[ -e $elem ]]; then
+         chmod -R 0777 $elem
+      fi
+   done
+}
 
-    # case Ubuntu
-    0)
-        echo "Installing for Ubuntu"
-        echo "Installing Dependencies"
-        apt-get install python-lxml python-django python-mutagen apache2 sqlite3 libapache2-mod-wsgi python-pysqlite2 ttf-dejavu ffmpeg
-        echo "Setting up Apache"
-        APACHE=$STANDARD_HTTP_USER
-        echo "Removing previous installations"
-        for elem in ${CREATE_DIRS[@]}; do
-            if [[ -e $elem ]]; then
-                rm -r $elem
-            fi
-        done
-        mv dist/server_cfg/apache $LAUDIO_CONF_DIR
-        mv dist/server_cfg/lighttpd $LAUDIO_CONF_DIR
-        rm -rf /etc/apache2/conf.d/laudio_apache.conf 
-        ln -s /etc/laudio/apache/laudio.conf /etc/apache2/conf.d/laudio_apache.conf 
-        echo "Creating Directories and installing laudio"
-        for elem in ${CREATE_DIRS[@]}; do
-            if ! [[ -e $elem ]]; then
-                mkdir -p $elem
-            fi
-            chown -R $APACHE:$APACHE $elem
-            chmod -R 0755 $elem
-        done
-        mv laudio $INSTALL_DIR
-        chown -R $APACHE:$APACHE $INSTALL_DIR
-        chmod -R 0755 $INSTALL_DIR
-        
-        echo "Symlinking fonts"
-        sudo rm $INSTALL_DIR/laudio/static/upload/themes/default/fonts/*
-        ln -s /usr/share/fonts/truetype/ttf-dejavu/DejaVuSans-Bold.ttf $INSTALL_DIR/laudio/static/upload/themes/default/font/DejaVuSans-Bold.ttf
-        ln -s /usr/share/fonts/truetype/ttf-dejavu/DejaVuSansCondensed.ttf $INSTALL_DIR/laudio/static/upload/themes/default/font/DejaVuSansCondensed.ttf
-        
-        echo "Creating Database"
-        python $INSTALL_DIR/laudio/manage.py syncdb --noinput
-        chown -R $APACHE:$APACHE $DATABASE_FILE
-        chmod -R 0755 $DATABASE_FILE
+function setup_production_rights {
+   for elem in ${needed_dirs[@]}; do
+      if [[ -e $elem ]]; then
+         chown -R $apache_ug:apache_ug $elem
+         chmod -R 0755 $elem
+      fi
+   done
+}
 
-        echo "Restarting apache"
-        /etc/init.d/apache2 restart
-    ;;
+# installs the dependencies on the required platform
+function install_deps {
+   if [ "$distro" -eq "fedora" ]; then 
+      yum install $fedora_deps
+   fi
 
-    # case Arch Linux
-    1)
-        echo "Installing for Arch Linux"
-        echo "Installing Dependencies"
-        pacman -S django python-lxml mutagen apache python-pysqlite sqlite3 mod_wsgi ttf-dejavu ffmpeg
-        echo "Setting up Apache"  
-        APACHE=$ARCH_HTTP_USER
-        # check if the entry is already there
-        conf=`grep -e laudio_apache.conf /etc/httpd/conf/httpd.conf`
-        if [ "$conf" = "" ]; then
-            echo "Include conf/extra/laudio_apache.conf" >> /etc/httpd/conf/httpd.conf
-        fi
-        # check if the mod_wsgi is enabled
-        conf=`grep -e LoadModule wsgi_module /etc/httpd/conf/httpd.conf`
-        if [ "$conf" = "" ]; then
-            echo "LoadModule wsgi_module modules/mod_wsgi.so" >> /etc/httpd/conf/httpd.conf
-        fi
-        echo "Removing previous installations"
-        for elem in ${CREATE_DIRS[@]}; do
-            if [[ -e $elem ]]; then
-                rm -r $elem
-            fi
-        done
-        mv dist/server_cfg/apache $LAUDIO_CONF_DIR
-        mv dist/server_cfg/lighttpd $LAUDIO_CONF_DIR
-        rm -rf /etc/httpd/conf/extra/laudio_apache.conf 
-        ln -s /etc/laudio/apache/laudio.conf /etc/httpd/conf/extra/laudio_apache.conf 
-        echo "Creating Directories and installing laudio"
-        for elem in ${CREATE_DIRS[@]}; do
-            if ! [[ -e $elem ]]; then
-                mkdir -p $elem
-            fi
-            chown -R $APACHE:$APACHE $elem
-            chmod -R 0755 $elem
-        done
-        mv laudio $INSTALL_DIR 
-        chown -R $APACHE:$APACHE $INSTALL_DIR
-        chmod -R 0755 $INSTALL_DIR
-        
-        echo "Creating Database"
-        python2 $INSTALL_DIR/laudio/manage.py syncdb --noinput
-        chown -R $APACHE:$APACHE $DATABASE_FILE
-        chmod -R 0755 $DATABASE_FILE
+   if [ "$distro" -eq "debian" ]; then 
+      apt-get install $debian_deps
+   fi
 
-        echo "Restarting Apache"
-        /etc/rc.d/httpd restart
+   if [ "$distro" -eq "arch" ]; then 
+      pacman -Sy $arch_deps
+   fi
 
-        echo "If you want to start Laudio at boot, add httpd to your DAEMONS in"
-        echo "/etc/rc.conf"
-    ;;
+   if [ "$distro" -eq "gentoo" ]; then 
+      emerge -av $gentoo_deps
+   fi
 
-    # case Gentoo
-    2)
-        echo "Installing for Gentoo"
-        echo "Installing Dependencies"
-        emerge -av dev-python/django dev-python/lxml media-libs/mutagen dev-python/pysqlite dev-db/sqlite www-apache/mod_wsgi www-servers/apache ttf-dejavu ffmpeg
-        echo "Setting up Apache"
-        APACHE=$STANDARD_HTTP_USER
-        echo "Removing previous installations"
-        for elem in ${CREATE_DIRS[@]}; do
-            if [[ -e $elem ]]; then
-                rm -r $elem
-            fi
-        done
-        mv dist/server_cfg/apache $LAUDIO_CONF_DIR
-        mv dist/server_cfg/lighttpd $LAUDIO_CONF_DIR
-        rm -rf /etc/apache2/vhosts.d/laudio_apache.conf
-        ln -s /etc/laudio/apache/laudio.conf /etc/apache2/vhosts.d/laudio_apache.conf 
-        echo "Creating Directories and installing laudio"
-        for elem in ${CREATE_DIRS[@]}; do
-            if ! [[ -e $elem ]]; then
-                mkdir -p $elem
-            fi
-            chown -R $APACHE:$APACHE $elem
-            chmod -R 0755 $elem
-        done
-        mv laudio $INSTALL_DIR
-        chown -R $APACHE:$APACHE $INSTALL_DIR
-        chmod -R 0755 $INSTALL_DIR
-        
-        echo "Creating Database"
-        python $INSTALL_DIR/laudio/manage.py syncdb --noinput
-        chown -R $APACHE:$APACHE $DATABASE_FILE
-        chmod -R 0755 $DATABASE_FILE
+   if [ "$distro" -eq "unknown" ]; then 
+      echo "Unknown Distribution! Please install the following dependencies"
+      echo $general_deps
+   fi
+}
 
-        echo "Restarting apache"
-        /etc/init.d/apache2 restart
-        echo "If you want to start Laudio at boot, add apache2 to your default"
-        echo "runlevel: rc-update add apache2 default"
-    ;;
+function install_configs {
+   mv dist/server_cfg/apache $config_dir
+   mv dist/server_cfg/lighttpd $config_dir
+   
+   if [ "$distro" -eq "fedora" ]; then 
+      ln -s /etc/laudio/apache/laudio.conf /etc/httpd/conf.d/laudio_apache.conf 
+   fi
 
-    *)
-        echo "Error: Unknown distribution, aborting"
-        exit 1    
-    ;;
-esac
+   if [ "$distro" -eq "debian" ]; then 
+      ln -s /etc/laudio/apache/laudio.conf /etc/apache2/conf.d/laudio_apache.conf 
+   fi
 
-exit 0
+   if [ "$distro" -eq "arch" ]; then 
+      ln -s /etc/laudio/apache/laudio.conf /etc/httpd/conf/extra/laudio_apache.conf 
+      echo "Include conf/extra/laudio_apache.conf" >> /etc/httpd/conf/httpd.conf
+   fi
+
+   if [ "$distro" -eq "gentoo" ]; then 
+      ln -s /etc/laudio/apache/laudio.conf /etc/apache2/vhosts.d/laudio_apache.conf 
+   fi
+
+}
+
+function symlink_fonts {
+   ln -s /usr/share/fonts/truetype/ttf-dejavu/DejaVuSans-Bold.ttf $install_dir/laudio/static/upload/themes/default/font/DejaVuSans-Bold.ttf
+   ln -s /usr/share/fonts/truetype/ttf-dejavu/DejaVuSansCondensed.ttf $install_dir/laudio/static/upload/themes/default/font/DejaVuSansCondensed.ttf
+}
+
+function restart_apache {
+   if [ "$distro" -eq "fedora" ]; then 
+      systemctl restart httpd.service
+   fi
+
+   if [ "$distro" -eq "debian" ]; then 
+      /etc/init.d/apache2 restart
+   fi
+
+   if [ "$distro" -eq "arch" ]; then 
+      /etc/rc.d/httpd restart
+   fi
+
+   if [ "$distro" -eq "gentoo" ]; then 
+      /etc/init.d/apache2 restart
+   fi
+
+   if [ "$distro" -eq "unknown" ]; then 
+      echo "Please restart your apache webserver!"
+   fi
+}
 
 
+# developement setup, dont use this in production!
+function devel_setup {
+   require_root
+   install_deps
+   create_dirs
+   setup_devel_rights
+   create_database
+   chmod 0777 $database_path
+}
+
+function production_setup {
+   require_root
+   install_deps
+   create_dirs
+   setup_production_rights
+   symlink_fonts
+   create_database
+   mv src $install_dir
+   setup_production_rights
+   restart_apache
+}
+
+
+# check for -r command to build and run program
+while getopts ":dp" opt; do
+case $opt in
+        d)
+            echo "Installing developement environment" >&2
+            devel_setup
+            ;;
+
+        p)
+            echo "Installing laudio" >&2
+            production_setup
+            ;;
+
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            ;;
+
+      esac
+done
+
+if [ $# -lt 1 ]; then
+   echo "Use -d for developement setup and -p for normal installation"
+fi
